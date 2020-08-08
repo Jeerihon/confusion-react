710b40de88d8acc20198e2ed5f3a9b47ebfa8a89
Artem Korytov <a.korytov@sitt.me>
2020-08-08 17:19:49 +0300

    redux init

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { Switch, Route, Redirect } from "react-router-dom";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+import { connect } from 'react-redux';
 
 import Home from "./HomeComponent";
 import About from "./AboutComponent";
@@ -9,21 +10,18 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import DishDetail from "./DishdetailComponent";
 
-import { DISHES } from "../shared/dishes";
-import { COMMENTS } from "../shared/comments";
-import { PROMOTIONS } from "../shared/promotions";
-import { LEADERS } from "../shared/leaders";
+const mapStateToProps = (state) => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders,
+  }
+}
 
 class Main extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      dishes: DISHES,
-      comments: COMMENTS,
-      promotions: PROMOTIONS,
-      leaders: LEADERS,
-    };
   }
 
   render() {
@@ -36,9 +34,9 @@ class Main extends Component {
             path="/home"
             component={() => (
               <Home
-                dish={this.state.dishes.find((dish) => dish.featured)}
-                promotion={this.state.promotions.find((promotion) => promotion.featured)}
-                leader={this.state.leaders.find((leader) => leader.featured)}
+                dish={this.props.dishes.find((dish) => dish.featured)}
+                promotion={this.props.promotions.find((promotion) => promotion.featured)}
+                leader={this.props.leaders.find((leader) => leader.featured)}
               />
             )}
           />
@@ -46,21 +44,21 @@ class Main extends Component {
           <Route
             exact
             path="/aboutus"
-            component={() => <About leaders={this.state.leaders} />}
+            component={() => <About leaders={this.props.leaders} />}
           />
 
           <Route
             exact
             path="/menu"
-            component={() => <Menu dishes={this.state.dishes} />}
+            component={() => <Menu dishes={this.props.dishes} />}
           />
 
           <Route
             path='/menu/:dishId'
             component={({ match }) => (
               <DishDetail
-                dish={this.state.dishes.find((dish) => dish.id === parseInt(match.params.dishId, 10))}
-                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
+                dish={this.props.dishes.find((dish) => dish.id === parseInt(match.params.dishId, 10))}
+                comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))}
               />
             )}
           />
@@ -76,4 +74,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
